5fbc7e8971b2757f4677d23c3120f82b61568e2d
Andrey Matukhno <andrey.matukhno@altexsoft.com>
2014-11-28 19:27:31 +0200

    djangotutorial home without class-based views

---
+++ one2one/one2one/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'one2one.views.home', name='home'),
+    # url(r'^one2one/', include('one2one.foo.urls')),
+    url(r'^polls/', include('polls.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
