5ebbcba0de35932fb69fdbf6cad17c1c223bad1d
mushmushroom <smirnitskayamp@gmail.com>
2014-11-30 14:06:21 +0200

    created polls application

---
+++ djangounchained/polls/models.py
@@ -0,0 +1,20 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
