4711799df2dea953b37e69af3eceedc2bf87ac66
goodniceweb <goodniceweb@gmail.com>
2014-11-30 22:09:08 +0200

    resolve chapter #2

--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,9 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
     def __unicode__(self):
         return self.question_text
 
