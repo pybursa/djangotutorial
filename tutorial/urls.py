7580297c1bf377d63dac45e2e9a63af2a923e062
goodniceweb <goodniceweb@gmail.com>
2014-11-30 23:00:18 +0200

    chapter #3

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tutorial.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
