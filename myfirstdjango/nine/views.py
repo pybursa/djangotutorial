af47709f82daed0f8a6649b7b14362de1cbdb681
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2014-12-03 19:38:30 +0200

    Next Commit

--- myfirstdjango/nine/views.py
+++ myfirstdjango/nine/views.py
@@ -5,12 +5,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 def nine_square(request):
-    r = render(request, 'square.html', {'testid': request.GET})
-    return r
+    if request.GET['a'] and request.GET['b'] and request.GET['c']:
+        discr = int(request.GET['b'])**2 - 4 * int(request.GET['a']) * int(request.GET['c'])
+        if discr > 0:
+            import math
+            x1 = (-int(request.GET['b']) + math.sqrt(discr)) / (2 * int(request.GET['a']))
+            x2 = (-int(request.GET['b']) - math.sqrt(discr)) / (2 * int(request.GET['a']))
+            r = render(request, 'square.html', {'x1': x1, 'x2': x2, 'testid': request.GET})
+        elif discr == 0:
+            x = -int(request.GET['b']) / (2 * (2 * int(request.GET['a'])))
+            r = render(request, 'square.html', {'x': x, 'testid': request.GET})
+        else:
+            error = "No root"
+            r = render(request, 'square.html', {'error': error, 'error': error})
+        return r
+    else:
+        r = render(request, 'square.html', {'testid': request.GET})
+        return r
 
 
 def nine_index(request):
     r = render(request, 'index.html')
     print type(r)
     return r
-
