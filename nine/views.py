4e621075a69421a4c8e3c0b20daed1f945348827
ellenaua <elenasharovar@gmail.com>
2014-12-04 00:08:26 +0200

    Display error if some parameter is not passed

--- nine/views.py
+++ nine/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 def square(request):
     params = request.GET.dict()
+    if not params['a']:
+        params['error'] = 'Please pass a in request'
+    if not params['b']:
+        params['error'] = 'Please pass b in request'
+    if not params['c']:
+        params['error'] = 'Please pass c in request'
     return render(request, 'square.html', params)
