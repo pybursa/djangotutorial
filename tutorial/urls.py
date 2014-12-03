eebe4a0f3b6143f5c91e55e8e14e9d2bc1299a4f
mushmushroom <smirnitskayamp@gmail.com>
2014-12-03 21:33:56 +0200

    added hw9

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 from nine.views import square
 
 urlpatterns = patterns('',
-
     url(r'^$', square),
     url(r'^nine/', include('nine.urls', namespace="nine")),
     url(r'^admin/', include(admin.site.urls)),
