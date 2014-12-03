9f2a5032d1f0165a0d83fcb0bbc1b0de5969ab87
ellenaua <elenasharovar@gmail.com>
2014-12-03 23:48:38 +0200

    Changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns(
     '',
     url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^nine/', include('nine.urls', namespace="nine")),
     url(r'^admin/', include(admin.site.urls)),
 )
