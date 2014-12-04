6a384f166a12c1f475882519e123f28bad8c77fb
wowkalucky <wowkalucky@gmail.com>
2014-12-04 17:05:54 +0200

    Added heroes views

--- nine/views.py
+++ nine/views.py
@@ -1,3 +1,6 @@
+import csv
+from datetime import datetime, date
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -31,7 +34,7 @@ def quad_equation(request):
     a = context_dict['a']
     b = context_dict['b']
     c = context_dict['c']
-    d = get_discr(**context_dict)
+    d = get_discr(a, b, c)
     if d < 0:
         return HttpResponse(result_message)
     else:
@@ -43,3 +46,57 @@ def quad_equation(request):
             result_message = "There are two roots: x1 = %g, x2 = %g" % (x1, x2)
     context_dict.update({'d': d, 'message': result_message})
     return render(request, 'nine/equations.html', context_dict)
+
+
+def heroes(request):
+    context = {}
+    context['heroes'] = get_heroes("nine/data.csv")
+    return render(request, 'nine/hero.html', context)
+
+
+def get_heroes(file_name, nick=None):
+    heroes_list = []
+    with open(file_name, 'r') as fr:
+        reader_dict = csv.DictReader(fr)
+        for row_dict in reader_dict:
+            surname = row_dict['surname']
+            name = row_dict['name']
+            birthdate = row_dict['birthdate']
+            nickname = row_dict['nickname'] or None
+            person = Person(surname, name, birthdate, nickname)
+            if nick is not None and nickname is not None:
+                if nick == nickname.lower():
+                    return person
+            heroes_list.append(person)
+    return heroes_list
+
+
+class Person(object):
+    """Represents notebook entry"""
+    def __init__(self, surname, firstname, birthdate, nickname=None):
+        self.surname = surname
+        self.first_name = firstname
+        if nickname is not None:
+            self.nickname = nickname
+        try:
+            date_format = "%Y-%m-%d"
+            datetime_object = datetime.strptime(birthdate, date_format)
+            self.birth_date = datetime_object.date()
+        except ValueError:
+            raise ValueError("You must provide birth date in correct format "
+                             "(YYYY-MM-DD)!")
+
+    def get_age(self):
+        today = date.today()
+        delta_in_days = today - self.birth_date
+        return str(int(delta_in_days.days // 365.25))
+
+    def get_fullname(self):
+        return "{0} {1}".format(self.surname, self. first_name)
+
+
+def hero(request, nick):
+    context = {}
+    hero_obj = get_heroes("nine/data.csv", nick)
+    context['superhero'] = hero_obj
+    return render(request, 'nine/hero.html', context)
