1cc908efbb9e67057f9282b85cbd643639835342
wowkalucky <wowkalucky@gmail.com>
2014-12-04 17:05:32 +0200

    Created heroes urls

--- nine/urls.py
+++ nine/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns(
     '',
     url(r'^$', RedirectView.as_view(pattern_name='nine:quadratic-equation')),
     url(r'square/$', 'nine.views.quad_equation', name='quadratic-equation'),
-
+    url(r'heroes/$', 'nine.views.heroes', name='justice-league'),
+    url(r'heroes/(?P<nick>\w+)$', 'nine.views.hero', name='personal-room'),
 )
